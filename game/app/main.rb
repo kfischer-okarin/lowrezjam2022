fa0672d0e5ef55a7e0de0a601dfe38f2dca491aa
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-08-01 23:45:49 +0900

    Add rendering canvas

--- game/app/main.rb
+++ game/app/main.rb
@@ -4,6 +4,20 @@ require 'lib/resources.rb'
 
 require 'app/resources.rb'
 
+SCREEN_W = 64
+SCREEN_H = 64
+
 def tick(args)
-  # Game
+  state = args.state
+  render(state, args.outputs)
+end
+
+def render(_state, outputs)
+  screen = outputs[:screen]
+  screen.background_color = [0x18, 0x14, 0x25]
+  screen.width = SCREEN_W
+  screen.height = SCREEN_H
+
+  outputs.background_color = [0, 0, 0]
+  outputs.primitives << { x: 288, y: 8, w: 704, h: 704, path: :screen }.sprite!
 end
