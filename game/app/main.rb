c169329832ceefbee0e99e60dc64b6aa77ba6d0f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-08-04 00:55:12 +0900

    Add run animation

--- game/app/main.rb
+++ game/app/main.rb
@@ -1,3 +1,4 @@
+require 'lib/animations.rb'
 require 'lib/debug_mode.rb'
 require 'lib/extra_keys.rb'
 require 'lib/resources.rb'
@@ -9,15 +10,64 @@ SCREEN_H = 64
 
 def tick(args)
   state = args.state
+  setup(state) if args.tick_count.zero?
   render(state, args.outputs)
 end
 
-def render(_state, outputs)
+def setup(state)
+  state.player = {
+    x: 0, y: 0,
+    state: :run,
+    face_direction: :right,
+  }
+  state.rendered_player = {
+    entity: state.player,
+    animations: load_player_animations,
+    sprite: {}.sprite!,
+    animation_type: nil,
+    animation_state: nil
+  }
+end
+
+def load_player_animations
+  Animations.read_asesprite_json('resources/character.json').tap { |animations|
+    right_animations = animations.keys.select { |key|
+      key.to_s.end_with?('_right')
+    }
+
+    right_animations.each do |key|
+      left_key = key.to_s.sub('_right', '_left').to_sym
+      animations[left_key] = Animations.flipped_horizontally animations[key]
+    end
+  }
+end
+
+def render(state, outputs)
   screen = outputs[:screen]
   screen.background_color = [0x18, 0x14, 0x25]
   screen.width = SCREEN_W
   screen.height = SCREEN_H
 
+  update_animation(state.rendered_player)
+  screen.primitives << state.rendered_player[:sprite]
+
   outputs.background_color = [0, 0, 0]
   outputs.primitives << { x: 288, y: 8, w: 704, h: 704, path: :screen }.sprite!
 end
+
+def update_animation(render_state)
+  entity = render_state[:entity]
+  animation_type = :"#{entity.state}_#{entity.face_direction}"
+  if render_state[:animation_type] == animation_type
+    Animations.next_tick render_state[:animation_state]
+    Animations.apply! render_state[:sprite], animation_state: render_state[:animation_state]
+  else
+    render_state[:animation_type] = animation_type
+    render_state[:animation_state] = Animations.start!(
+      render_state[:sprite],
+      animation: render_state[:animations][animation_type]
+    )
+  end
+end
+
+$gtk.reset
---
+++ game/lib/animations.rb
@@ -0,0 +1,127 @@
+module Animations
+  class << self
+    def read_asesprite_json(path)
+      sprite_sheet_data = deep_symbolize_keys! $gtk.parse_json_file(path)
+
+      frames = sprite_sheet_data.fetch :frames
+      first_frame = frames.first.fetch :frame
+      last_slash_index = path.rindex '/'
+      base = {
+        w: first_frame[:w],
+        h: first_frame[:h],
+        path: path[0..last_slash_index] + sprite_sheet_data.fetch(:meta).fetch(:image)
+      }
+
+      {}.tap { |result|
+        tags = sprite_sheet_data.fetch(:meta).fetch(:frameTags).map { |frame_tag_data| frame_tag_data.fetch(:name) }
+        tags.each do |tag|
+          sorted_animation_frames = frames.select { |frame_data|
+            frame_data.fetch(:filename).start_with?("#{tag}-----") # avoid frames with same prefix
+          }.sort_by(&:filename)
+
+          result[tag.to_sym] = build(
+            frames: sorted_animation_frames.map { |frame_data|
+              frame = frame_data.fetch(:frame)
+              {
+                x: frame[:x],
+                y: frame[:y],
+                ticks: frame_data.fetch(:duration).idiv(50) * 3 # 50ms = 3 ticks
+              }
+            },
+            **base
+          )
+        end
+      }
+    end
+
+    def build(frames:, **base)
+      {
+        base: {
+          flip_horizontally: false,
+          tile_w: base[:w],
+          tile_h: base[:h]
+        }.merge(base),
+        frames: frames
+      }
+    end
+
+    def flipped_horizontally(animation)
+      {
+        base: animation[:base].merge(flip_horizontally: true),
+        frames: animation[:frames]
+      }
+    end
+
+    def start!(primitive, animation:, repeat: true)
+      {
+        animation: animation,
+        frame_index: 0,
+        ticks: 0,
+        repeat: repeat,
+        finished: false
+      }.tap { |animation_state|
+        primitive.merge! animation[:base]
+        apply! primitive, animation_state: animation_state
+      }
+    end
+
+    def apply!(primitive, animation_state:)
+      frame = animation_state[:animation][:frames][animation_state[:frame_index]]
+      primitive[:tile_x] = frame[:x]
+      primitive[:tile_y] = frame[:y]
+    end
+
+    def next_tick(animation_state)
+      return if finished? animation_state
+
+      frames = animation_state[:animation][:frames]
+
+      animation_state[:ticks] += 1
+      if animation_state[:ticks] >= frames[animation_state[:frame_index]][:ticks]
+        animation_state[:ticks] = 0
+
+        if animation_state[:frame_index] < frames.length - 1
+          animation_state[:frame_index] += 1
+          return
+        end
+
+        if animation_state[:repeat]
+          animation_state[:frame_index] = 0
+        else
+          animation_state[:finished] = true
+        end
+      end
+    end
+
+    def finished?(animation_state)
+      animation_state[:finished]
+    end
+
+    private
+
+    def deep_symbolize_keys!(value)
+      case value
+      when Hash
+        symbolize_keys!(value)
+        value.each_value do |hash_value|
+          deep_symbolize_keys!(hash_value)
+        end
+      when Array
+        value.each do |array_value|
+          deep_symbolize_keys!(array_value)
+        end
+      end
+
+      value
+    end
+
+    def symbolize_keys!(hash)
+      hash.each_key do |key|
+        next unless key.is_a? String
+
+        hash[key.to_sym] = hash.delete(key)
+      end
+      hash
+    end
+  end
+end
