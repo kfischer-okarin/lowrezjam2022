331dc828b9ea4341ff62c3b24a912d648043d8f2
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-08-07 15:41:02 +0900

    Extract Colliders DSL mixin

--- game/tests/test_helpers.rb
+++ game/tests/test_helpers.rb
@@ -24,17 +24,30 @@ module TestHelpers
         @args.tick_count += 1
       end
     end
+
+    module Colliders
+      def initialize(args)
+        super
+
+        @args.state.colliders = [
+          { collider: { x: -1000, y: -5, w: 2000, h: 5 } }
+        ]
+      end
+
+      def collider_at(x:, y:, w:, h:)
+        @args.state.colliders << { collider: { x: x, y: y, w: w, h: h } }
+      end
+    end
   end
 
-
   class PlayerTestDSL < DSL::Base
+    include DSL::Colliders
+
     attr_reader :player, :last_input_actions
 
     def initialize(args)
       super
-      @args.state.colliders = [
-        { collider: { x: -1000, y: -5, w: 2000, h: 5 } }
-      ]
+
       @player = Player.build
       @initial_attributes = nil
     end
@@ -44,10 +57,6 @@ module TestHelpers
       @initial_attributes.each do |attribute, value|
         player[attribute] = value.dup
       end
-    end
-
-    def collider_at(x:, y:, w:, h:)
-      @args.state.colliders << { collider: { x: x, y: y, w: w, h: h } }
     end
 
     def input(actions)
