7fe924e8b310bad57602bc7d43d697de61345f06
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-08-11 19:04:13 +0900

    Refactor asesprite json read

--- game/lib/animations/asesprite_json.rb
+++ game/lib/animations/asesprite_json.rb
@@ -4,20 +4,12 @@ module Animations
       def read(path)
         sprite_sheet_data = deep_symbolize_keys! $gtk.parse_json_file(path)
 
-        frames = sprite_sheet_data.fetch :frames
-        first_frame = frames.first.fetch :frame
-        last_slash_index = path.rindex '/'
-        base = {
-          w: first_frame[:w],
-          h: first_frame[:h],
-          tile_w: first_frame[:w],
-          tile_h: first_frame[:h],
-          flip_horizontally: false,
-          path: path[0..last_slash_index] + sprite_sheet_data.fetch(:meta).fetch(:image)
-        }
+        base = build_base(sprite_sheet_data, path)
 
         {}.tap { |result|
+          frames = sprite_sheet_data.fetch :frames
           slices_data = sprite_sheet_data.fetch(:meta).fetch :slices
+
           sprite_sheet_data.fetch(:meta).fetch(:frameTags).each do |frame_tag_data|
             tag = frame_tag_data.fetch(:name).to_sym
             frame_range = frame_tag_data.fetch(:from)..frame_tag_data.fetch(:to)
@@ -30,15 +22,7 @@ module Animations
                   tile_y: frame[:y],
                   duration: frame_data.fetch(:duration).idiv(50) * 3, # 50ms = 3 ticks
                   metadata: {
-                    slices: {}.tap { |slices|
-                      slices_data.each do |slice_data|
-                        name = slice_data.fetch(:name).to_sym
-                        key_frame = slice_data[:keys].select { |slice_key_data|
-                          slice_key_data.fetch(:frame) <= frame_index
-                        }.last
-                        slices[name] = key_frame.fetch(:bounds)
-                      end
-                    }
+                    slices: slice_bounds_for_frame(slices_data, frame_index)
                   }
                 }
               },
@@ -49,6 +33,32 @@ module Animations
       end
 
       private
+
+      def build_base(sprite_sheet_data, path)
+        frames = sprite_sheet_data.fetch :frames
+        first_frame = frames.first.fetch :frame
+        last_slash_index = path.rindex '/'
+        {
+          w: first_frame[:w],
+          h: first_frame[:h],
+          tile_w: first_frame[:w],
+          tile_h: first_frame[:h],
+          flip_horizontally: false,
+          path: path[0..last_slash_index] + sprite_sheet_data.fetch(:meta).fetch(:image)
+        }
+      end
+
+      def slice_bounds_for_frame(slices_data, frame_index)
+        {}.tap { |slices|
+          slices_data.each do |slice_data|
+            name = slice_data.fetch(:name).to_sym
+            key_frame = slice_data[:keys].select { |slice_key_data|
+              slice_key_data.fetch(:frame) <= frame_index
+            }.last
+            slices[name] = key_frame.fetch(:bounds)
+          end
+        }
+      end
 
       def deep_symbolize_keys!(value)
         case value
