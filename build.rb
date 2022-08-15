a77edd774b661cf467b64cc553e45aafbc00773a
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2022-08-15 23:55:14 +0900

    Improve build script

--- build.rb
+++ build.rb
@@ -7,7 +7,7 @@ require 'rubygems'
 require 'bundler/setup'
 require 'zip'
 
-GAME_FOLDER_NAME = 'game'
+GAME_FOLDER_NAME = 'game'.freeze
 
 def main
   build_game
@@ -20,7 +20,7 @@ def build_game
 end
 
 def build_with_smaug
-  system "smaug build -p #{GAME_FOLDER_NAME}"
+  system "smaug build -p #{game_folder}"
 end
 
 # Create release zip files
@@ -36,19 +36,19 @@ class ZipCreator
     create_windows_zip
     create_macos_zip
     create_linux_zip
-    move_html5_zip
+    create_html5_zip
   end
 
   private
 
   def read_metadata
-    metadata = File.readlines(Pathname(GAME_FOLDER_NAME) / 'metadata' / 'game_metadata.txt', chomp: true)
-    version_line = metadata.find { |line| line.include? 'version' }
-    gameid_line = metadata.find { |line| line.include? 'gameid' }
+    metadata_lines = File.readlines(game_folder / 'metadata' / 'game_metadata.txt', chomp: true)
 
-    {
-      gameid: gameid_line.split('=')[1],
-      version: version_line.split('=')[1]
+    {}.tap { |metadata|
+      %i[version gameid].each do |key|
+        line = metadata_lines.find { |l| l.include? key.to_s }
+        metadata[key] = line.split('=')[1].strip
+      end
     }
   end
 
@@ -58,25 +58,31 @@ class ZipCreator
   end
 
   def create_windows_zip
-    zip_file = Pathname("dist/#{@gameid}-windows-v#{@version}.zip")
+    zip_file = dist_file("#{@gameid}-windows-v#{@version}.zip")
     Zip::File.open(zip_file, Zip::File::CREATE) do |zipfile|
       zipfile.add("#{@gameid}.exe", built_file("#{@gameid}-windows-amd64.exe"))
     end
   end
 
   def create_macos_zip
-    FileUtils.mv(built_file("#{@gameid}-macos.zip"), "dist/#{@gameid}-macos-v#{@version}.zip")
+    FileUtils.mv(
+      built_file("#{@gameid}-macos.zip"),
+      dist_file("#{@gameid}-macos-v#{@version}.zip")
+    )
   end
 
   def create_linux_zip
-    zip_file = Pathname("dist/#{@gameid}-linux-v#{@version}.zip")
+    zip_file = dist_file("#{@gameid}-linux-v#{@version}.zip")
     Zip::File.open(zip_file, Zip::File::CREATE) do |zipfile|
       zipfile.add("#{@gameid}.bin", built_file("#{@gameid}-linux-amd64.bin"))
     end
   end
 
-  def move_html5_zip
-    FileUtils.mv(built_file("#{@gameid}-html5.zip"), "dist/#{@gameid}-html5.zip")
+  def create_html5_zip
+    FileUtils.mv(
+      built_file("#{@gameid}-html5.zip"),
+      dist_file("#{@gameid}-html5.zip")
+    )
   end
 end
 
@@ -86,7 +92,15 @@ def remove_build_directories
 end
 
 def built_file(filename)
-  Pathname(GAME_FOLDER_NAME) / 'builds' / filename
+  game_folder / 'builds' / filename
+end
+
+def dist_file(filename)
+  Pathname('dist') / filename
+end
+
+def game_folder
+  Pathname(GAME_FOLDER_NAME)
 end
 
 main if $PROGRAM_NAME == __FILE__
